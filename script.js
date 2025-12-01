4b13eb3d1eea5c7641895dc8da6badb5980b0702
Ulukbek <sherqzx@gmail.com>
2025-12-01 16:09:33 +0600

    init commit

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-// function ColorFon (FonValue) {
-//     const value = prompt('введите цвет',FonValue)
-//     document.body.style.backgroundColor = value;
-// }
-
-// ColorFon()
-
-
-// function TraficColor (color) {
-//     const colorValue = prompt ('Введите цвет светафора',color )
-//     document.querySelector("div").style.backgroundColor = colorValue;
-//     if(colorValue == 'green') {
-//         document.querySelector("div").innerText = "GO"
-//     }
-//     else if (colorValue == 'red') {
-//         document.querySelector("div").innerText = "STOP"
-//     }
-//     else if (colorValue == 'yellow') {
-//         document.querySelector("div").innerText = "WAIT"
-//     }
-//     else document.querySelector('div').innerText = "Неверный цвет"
-// }
-// TraficColor()
-
 
 
 const IncrimentElement = document.querySelector('.increment')
